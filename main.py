d0f18f47378fc7d4f0b7a14cb125ac7338d2c15a
bednarskamartyna <martyna.krol94@gmail.com>
2022-04-04 20:03:32 +0200

    3

--- main.py
+++ main.py
@@ -187,3 +187,15 @@ class Trapez(Figura):
 trapez = Trapez(3,4,4)
 trapez.obwod()
 trapez.pole()
+
+class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+class Bus (Vehicle):
+    pass
+
+bus = Bus("Szkolny bus", 180, 12)
+
